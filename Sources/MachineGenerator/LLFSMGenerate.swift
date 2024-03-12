cf2c12e04ed62e6082e4ba9d6e8a12eedc34fcac
Morgan McColl <morgan.mccoll92@gmail.com>
2024-03-12 14:19:11 +1000

    Added version to main binary.

--- Sources/MachineGenerator/LLFSMGenerate.swift
+++ Sources/MachineGenerator/LLFSMGenerate.swift
@@ -64,6 +64,7 @@ struct LLFSMGenerate: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "llfsmgenerate",
         abstract: "A utility for performing operations on LLFSM formats.",
+        version: "1.1.0",
         subcommands: [Generate.self, VHDLGenerator.self]
     )
 
