3e92d23612f38aec1864c2b6ee5dd048af522a12
Morgan McColl <morgan.mccoll92@gmail.com>
2024-04-03 02:41:24 +1000

    Update swift_helpers to version 2.0.1.

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .package(url: "https://github.com/mipalgu/VHDLParsing", from: "2.4.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
         .package(url: "https://github.com/mipalgu/VHDLKripkeStructureGenerator.git", from: "0.1.2"),
-        .package(url: "https://github.com/mipalgu/swift_helpers.git", from: "2.0.0")
+        .package(url: "https://github.com/mipalgu/swift_helpers.git", from: "2.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
