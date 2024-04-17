38342275662f25e7b481be10fc26764116d5ece9
Morgan McColl <morgan.mccoll92@gmail.com>
2024-04-17 16:04:16 +1000

    Removed old targets and depend on new VHDLJSModels package.

--- Package.swift
+++ Package.swift
@@ -5,17 +5,13 @@ import PackageDescription
 
 /// Package definition.
 let package = Package(
-    name: "VHDLMachineTransformations",
+    name: "LLFSMGenerate",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other
         // packages.
-        .library(
-            name: "VHDLMachineTransformations",
-            targets: ["VHDLMachineTransformations", "JavascriptModel"]
-        ),
         .executable(
             name: "llfsmgenerate",
-            targets: ["MachineGenerator", "VHDLMachineTransformations", "JavascriptModel"]
+            targets: ["MachineGenerator"]
         )
     ],
     dependencies: [
@@ -24,35 +20,23 @@ let package = Package(
         .package(url: "https://github.com/mipalgu/VHDLParsing", from: "2.4.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
         .package(url: "https://github.com/mipalgu/VHDLKripkeStructureGenerator.git", from: "0.2.2"),
-        .package(url: "https://github.com/CPSLabGU/SwiftUtils.git", from: "0.1.0")
+        .package(url: "https://github.com/CPSLabGU/SwiftUtils.git", from: "0.1.0"),
+        .package(url: "https://github.com/CPSLabGU/VHDLJSModels", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "JavascriptModel"
-        ),
         .executableTarget(
             name: "MachineGenerator",
             dependencies: [
-                .target(name: "JavascriptModel"),
-                .target(name: "VHDLMachineTransformations"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "VHDLMachines", package: "VHDLMachines"),
                 .product(name: "VHDLParsing", package: "VHDLParsing"),
                 .product(name: "VHDLKripkeStructureGenerator", package: "VHDLKripkeStructureGenerator"),
-                .product(name: "SwiftUtils", package: "SwiftUtils")
+                .product(name: "SwiftUtils", package: "SwiftUtils"),
+                .product(name: "VHDLJSModels", package: "VHDLJSModels")
             ]
         ),
-        .target(
-            name: "VHDLMachineTransformations",
-            dependencies: [
-                .target(name: "JavascriptModel"),
-                .product(name: "VHDLMachines", package: "VHDLMachines"),
-                .product(name: "VHDLParsing", package: "VHDLParsing")
-            ]
-        ),
-        .testTarget(name: "JavascriptModelTests", dependencies: ["JavascriptModel"]),
         .testTarget(
             name: "MachineGeneratorTests",
             dependencies: [
@@ -64,18 +48,13 @@ let package = Package(
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "VHDLKripkeStructureGenerator", package: "VHDLKripkeStructureGenerator"),
                 .product(name: "SwiftUtils", package: "SwiftUtils"),
+                .product(name: "VHDLJSModels", package: "VHDLJSModels"),
                 "TestHelpers"
             ]
         ),
         .testTarget(
             name: "TestHelpers",
             dependencies: ["VHDLMachineTransformations", "VHDLParsing", "VHDLMachines", "JavascriptModel"]
-        ),
-        .testTarget(
-            name: "VHDLMachineTransformationsTests",
-            dependencies: [
-                "VHDLMachineTransformations", "VHDLParsing", "VHDLMachines", "JavascriptModel", "TestHelpers"
-            ]
         )
     ]
 )
