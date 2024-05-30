c4262ec74b5b27a15ae21d707d0b787c570056af
Morgan McColl <morgan.mccoll92@gmail.com>
2024-05-30 11:32:49 +1000

    Bumped version number.

--- Sources/MachineGenerator/LLFSMGenerate.swift
+++ Sources/MachineGenerator/LLFSMGenerate.swift
@@ -64,7 +64,7 @@ struct LLFSMGenerate: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "llfsmgenerate",
         abstract: "A utility for performing operations on LLFSM formats.",
-        version: "2.0.0",
+        version: "2.0.1",
         subcommands: [Generate.self, VHDLGenerator.self, CleanCommand.self, InstallCommand.self]
     )
 
