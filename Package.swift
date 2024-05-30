ce16cc1356a418b7e7f43f238c1de031b930d2ec
Morgan McColl <morgan.mccoll92@gmail.com>
2024-05-31 00:18:18 +1000

    Now depend on VHDLKripkeStructures.

--- Package.swift
+++ Package.swift
@@ -21,7 +21,8 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
         .package(url: "https://github.com/mipalgu/VHDLKripkeStructureGenerator.git", from: "0.3.1"),
         .package(url: "https://github.com/CPSLabGU/SwiftUtils.git", from: "0.1.0"),
-        .package(url: "https://github.com/CPSLabGU/VHDLJSModels", from: "1.0.0")
+        .package(url: "https://github.com/CPSLabGU/VHDLJSModels", from: "1.0.0"),
+        .package(url: "https://github.com/CPSLabGU/VHDLKripkeStructures", from: "1.2.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -34,7 +35,8 @@ let package = Package(
                 .product(name: "VHDLParsing", package: "VHDLParsing"),
                 .product(name: "VHDLKripkeStructureGenerator", package: "VHDLKripkeStructureGenerator"),
                 .product(name: "SwiftUtils", package: "SwiftUtils"),
-                .product(name: "VHDLJSModels", package: "VHDLJSModels")
+                .product(name: "VHDLJSModels", package: "VHDLJSModels"),
+                .product(name: "VHDLKripkeStructures", package: "VHDLKripkeStructures")
             ]
         ),
         .testTarget(
@@ -47,7 +49,8 @@ let package = Package(
                 .product(name: "VHDLKripkeStructureGenerator", package: "VHDLKripkeStructureGenerator"),
                 .product(name: "SwiftUtils", package: "SwiftUtils"),
                 .product(name: "VHDLJSModels", package: "VHDLJSModels"),
-                "TestHelpers"
+                "TestHelpers",
+                .product(name: "VHDLKripkeStructures", package: "VHDLKripkeStructures")
             ]
         ),
         .testTarget(
